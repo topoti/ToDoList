1fe4ee9c65a7d3e5735c9abad074d11762fbee05
topoti <ts03351@gmail.com>
2023-05-29 15:56:55 +0600

    commit

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ var taskListArray= [];
 
 function saveTask(){
     debugger;
-   
+   var taskName = ducument.getElementById("textItem").value;
+   var todoObject = {
+    taskId: taskListArray.length + 1,
+    taskName: taskName
+   };
+   taskListArray.push(todoObject);
 }
